7a7c93ca3aea83df4ad8158fb803a3a96171b07d
yuan00yi <70881616+yuan00yi@users.noreply.github.com>
2023-07-27 18:46:35 +0800

    app_edtunnel.js

--- app_edtunnel.js
+++ app_edtunnel.js
@@ -4,7 +4,7 @@ const net = require('net');
 
 // 创建 WebSocket 服务器
 const wss = new WebSocket.Server({ port: 443 });
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = 'b3da8d87-e89d-4376-a009-06f52c742720';
 let proxyIP = "64.68.192." + Math.floor(Math.random() * 255);
 
 let address = '';
